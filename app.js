769da126dfa3a6f56b00a544252b9f6ef4475330
Pragades Rajagopal <66718413+Pragades-Rajagopal@users.noreply.github.com>
2023-03-28 16:45:47 +0000

    commit-1

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+const PORT = 8000;
+
+app.get('/hello', (request, response) => {
+    response.send("hello world!")
+});
+
+app.get('/name', (request, response) => {
+    response.send("hello Max!")
+});
+
+app.get('/goodbye', (request, response) => {
+    response.send("thanks for visiting")
+});
+
+app.listen(PORT, () => {
+    console.log(`server is running in port:${PORT}`);
+})
